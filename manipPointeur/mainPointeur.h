a026977b5a09ec64f91dab998aa51ac043702725
Hrach <144778919+HrachAT@users.noreply.github.com>
2025-05-12 10:30:14 +0200

    Add files via upload

---
+++ manipPointeur/mainPointeur.h
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cFiles/file.h to edit this template
+ */
+
+/* 
+ * File:   mainPointeur.h
+ * Author: hatomyan
+ *
+ * Created on 13 janvier 2025, 12:38
+ */
+
+#ifndef MAINPOINTEUR_H
+#define MAINPOINTEUR_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* MAINPOINTEUR_H */
+
