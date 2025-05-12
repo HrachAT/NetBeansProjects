ca99f73426a9b7504f218376a4a4a0b9da88c47f
Hrach <144778919+HrachAT@users.noreply.github.com>
2025-05-12 10:32:35 +0200

    Add files via upload

---
+++ Tableau_de_chaines/main.c
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cFiles/main.c to edit this template
+ */
+
+/* 
+ * File:   main.c
+ * Author: hatomyan
+ *
+ * Created on 2 décembre 2024, 11:41
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#define NBMOTS 3
+#define LONGMAXMOT 10
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    char tabChaine[NBMOTS][LONGMAXMOT]=("coucou","salut","hello");
+    
+    return (EXIT_SUCCESS);
+}
+
---
+++ structures/main.c
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cFiles/main.c to edit this template
+ */
+
+/* 
+ * File:   main.c
+ * Author: hatomyan
+ *
+ * Created on 3 février 2025, 15:47
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
